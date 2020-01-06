fe07637db4c1149f5fdbf1ddc7ae6ef57a846089
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2020-01-06 14:23:27 +0700

    Update simple_read.php

--- php/simple_read.php
+++ php/simple_read.php
@@ -1,5 +1,12 @@
 <?php
 class dataRes{}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHP_Mailer/Exception.php';
+require 'PHP_Mailer/PHPMailer.php';
+require 'PHP_Mailer/SMTP.php';
+$mail = new PHPMailer(true);   
 
 $dataBaru = new DataRes;
 
